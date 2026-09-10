31bd1f57cc139c64f3302101a85dce34f1211345
jianchu-in <19837111824wjy@gmail.com>
2026-09-10 22:58:29 +0800

    fix: keep story app export type-safe

--- components/story/story-app.tsx
+++ components/story/story-app.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import type { ComponentProps } from "react";
 import { StoryApp as StoryAppBase } from "./story-app-base";
 import { StoryScrollFix } from "./story-scroll-fix";
 
-export function StoryApp(props: React.ComponentProps<typeof StoryAppBase>) {
+export function StoryApp(props: ComponentProps<typeof StoryAppBase>) {
   return (
     <>
       <StoryScrollFix />
