59926cb86203ea6652b7a63ef3288e91085ed09a
jianchu-in <19837111824wjy@gmail.com>
2026-09-10 22:18:03 +0800

    fix(story): apply reliable touch scroll behavior

--- components/story/story-app.tsx
+++ components/story/story-app.tsx
@@ -1,3 +1,13 @@
 "use client";
 
-export { StoryApp } from "./story-app-base";
+import { StoryApp as StoryAppBase } from "./story-app-base";
+import { StoryScrollFix } from "./story-scroll-fix";
+
+export function StoryApp(props: React.ComponentProps<typeof StoryAppBase>) {
+  return (
+    <>
+      <StoryScrollFix />
+      <StoryAppBase {...props} />
+    </>
+  );
+}
